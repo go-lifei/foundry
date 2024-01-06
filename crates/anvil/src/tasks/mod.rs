97baffd7d0857b53181ff6c3059e6cc855ac3873
lifei <lifei.nft@gmail.com>
2024-01-07 00:07:08 +0800

    filecoin

--- crates/anvil/src/tasks/mod.rs
+++ crates/anvil/src/tasks/mod.rs
@@ -5,7 +5,7 @@ use anvil_core::types::Forking;
 use ethers::{
     prelude::Middleware,
     providers::{JsonRpcClient, PubsubClient},
-    types::{Block, H256},
+    types::{Block, Transaction,H256},
 };
 use std::{fmt, future::Future};
 use tokio::{runtime::Handle, task::JoinHandle};
@@ -141,7 +141,7 @@ impl TaskManager {
     where
         P: Middleware + Unpin + 'static,
         <P as Middleware>::Provider: PubsubClient,
-        F: Fn(Block<H256>) -> Fut + Unpin + Send + Sync + 'static,
+        F: Fn(Block<Transaction>) -> Fut + Unpin + Send + Sync + 'static,
         Fut: Future<Output = ()> + Send,
     {
         let shutdown = self.on_shutdown.clone();
